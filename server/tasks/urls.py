d74b017dd5b24e41d4f7ac4d165de653e5a9be08
SntgRM <santy01052007@gmail.com>
2026-05-27 18:56:42 -0500

    ADD - create urls.py for task management with list and detail views

---
+++ server/tasks/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import (
+    TaskListCreateView,
+    TaskDetailView
+)
+
+urlpatterns = [
+    path('', TaskListCreateView.as_view()),
+    path('<int:pk>/', TaskDetailView.as_view()),
+]
